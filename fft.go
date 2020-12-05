a607dc2fdcb02a3a5d1d19913b912f2527b78327
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2020-12-05 20:14:42 +0900

    Add fft

--- fft.go
+++ fft.go
@@ -1,6 +1,6 @@
 package spat
 
-type T float64
+type T = complex128
 
 func SizedDFT(array, temp, omega []complex128, size, n int) {
 	var m int = size / n
@@ -37,5 +37,43 @@ func IDFT(array []complex128, size int) {
 	SizedDFT(array, temp, omega, size, -1)
 	for i := 0; i < size; i++ {
 		array[i] = complex(real(array[i])/float64(size), imag(array[i])/float64(size), );
+	}
+}
+
+func fft2(array []T, temp, omega []complex128, size int) {
+	n := 1
+	for ; n < size; n <<= 1 {
+		m := size / n;
+		q := m >> 1;
+		/*permutation*/
+		for i := 0; i < n; i++ {
+			for k := 0; k < q; k++ {
+				temp[i*m+k] = array[i*m+2*k]
+				temp[i*m+q+k] = array[i*m+2*k+1]
+			}
+		}
+		for i := 0; i < size; i++ {
+			array[i] = temp[i];
+		}
+	}
+	for ; n > 1; n >>= 1 {
+		m := size / n;
+		q := n >> 1;
+		r := size / q;
+		/*adding up with twiddle factors*/
+		for i := 0; i < size; i++ {
+			temp[i] = 0;
+		}
+		for i := 0; i < q; i++ {
+			for k := 0; k < m; k++ {
+				temp[2*m*i+k] += array[2*m*i+k];
+				temp[2*m*i+m+k] += array[2*m*i+k];
+				temp[2*m*i+k] += array[2*m*i+m+k] * omega[(k%r)*q];
+				temp[2*m*i+m+k] += array[2*m*i+m+k] * omega[((m+k)%r)*q];
+			}
+		}
+		for i := 0; i < size; i++ {
+			array[i] = temp[i];
+		}
 	}
 }
