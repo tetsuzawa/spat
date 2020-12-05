19d8c6bb0261f0207229e6bca04a7fc58a2d111f
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2020-12-05 21:01:45 +0900

    Add FFTPrime

--- fft.go
+++ fft.go
@@ -40,11 +40,11 @@ func IDFT(array []complex128, size int) {
 	}
 }
 
-func fft2(array []T, temp, omega []complex128, size int) {
+func SizedFFT2(array []T, temp, omega []complex128, size int) {
 	n := 1
 	for ; n < size; n <<= 1 {
-		m := size / n;
-		q := m >> 1;
+		m := size / n
+		q := m >> 1
 		/*permutation*/
 		for i := 0; i < n; i++ {
 			for k := 0; k < q; k++ {
@@ -53,16 +53,16 @@ func fft2(array []T, temp, omega []complex128, size int) {
 			}
 		}
 		for i := 0; i < size; i++ {
-			array[i] = temp[i];
+			array[i] = temp[i]
 		}
 	}
 	for ; n > 1; n >>= 1 {
-		m := size / n;
-		q := n >> 1;
-		r := size / q;
+		m := size / n
+		q := n >> 1
+		r := size / q
 		/*adding up with twiddle factors*/
 		for i := 0; i < size; i++ {
-			temp[i] = 0;
+			temp[i] = 0
 		}
 		for i := 0; i < q; i++ {
 			for k := 0; k < m; k++ {
@@ -76,4 +76,134 @@ func fft2(array []T, temp, omega []complex128, size int) {
 			array[i] = temp[i];
 		}
 	}
+}
+
+func FFT2(array, temp, omega []complex128) {
+	SizedFFT2(array, temp, omega, len(array))
+}
+
+func FFTPrime(array []T, size, n, sign int) {
+	p := size / n
+	length := p - 1
+	power2 := true
+	i := 0;
+	for ; length > 1; length >>= 1 {
+		if length&1 != 0 {
+			power2 = false;
+		}
+		i++
+	}
+	if power2 {
+		length = p - 1
+	} else {
+		length = 1 << (i + 2)
+	}
+	pad := length - (p - 1)
+	g := primePrimitiveRoot(p)
+	ig := powerMod(g, p-2, p)
+	data := make([]complex128, length)
+	temp := make([]complex128, length)
+	omega := make([]complex128, length)
+	omega2 := make([]complex128, length)
+
+	for i := 0; i < length; i++ {
+		//omega[i].real(fft_cos(i, length));
+		//omega[i].imag(-fft_sin(i, length));
+		omega[i] = complex(FFTCos(i, length), -FFTSin(i, length))
+
+		//omega2[i].real(fft_cos(powerMod(ig, i % (p - 1), p), p));
+		//omega2[i].imag(sign * fft_sin(powerMod(ig, i % (p - 1), p), p));
+		omega2[i] = complex(FFTCos(powerMod(ig, i%(p-1), p), p), sign*FFTSin(powerMod(ig, i%(p-1), p), p))
+	}
+	FFT2(omega2, temp, omega)
+	for i := 0; i < n; i++ {
+		data[0] = array[i*p+1]
+		for j := 1; j < length; j++ {
+			if j <= pad {
+				data[j] = 0
+			} else {
+				data[j] = array[i*p+powerMod(g, j-pad, p)];
+			}
+		}
+		/*===Convolution theorem===*/
+		FFT2(data, temp, omega)
+		for j := 0; j < length; j++ {
+			temp[j] = data[j] * omega2[j];
+		}
+		for j := 0; j < length; j++ {
+			data[j] = temp[j]
+			omega[i] = complex(real(omega[i]), imag(-omega[i]))
+
+		}
+		FFT2(data, temp, omega)
+		/*add array[i*p] term*/
+		for j := 0; j < length; j++ {
+			//data[j].real(data[j].real() /length + array[i * p].real());
+			//data[j].imag(data[j].imag() /length + array[i * p].imag());
+			data[j] = complex(real(data[j])/float64(length)+real(array[i*p]), imag(data[j])/float64(length)+imag(array[i*p]))
+
+		}
+		/*===Convolution theorem end===*/
+
+		/*DC term*/
+		temp[0] = 0;
+		for j := 0; j < p; j++ {
+			temp[0] += array[i*p+j];
+		}
+		array[i*p] = temp[0];
+
+		/*non-DC term*/
+		for j := 0; j < p-1; j++ {
+			array[i*p+powerMod(ig, j, p)] = data[j];
+		}
+	}
+}
+
+func primePrimitiveRoot(p int) int {
+	var i, t int
+	//std::vector<int> list;
+	var list []int
+
+	t = p - 1
+	for i = 2; i*i <= t; i++ {
+		if t%i == 0 {
+			//list.push_back(i);
+			list = append(list, i)
+			//do {
+			//t /= i;
+			//} while (t % i == 0);
+			t /= i
+			for t%i == 0 {
+				t /= i
+			}
+		}
+	}
+
+	//for (std::vector<int>::iterator it = list.begin(); it != list.end(); it++) {
+	//*it = (p - 1) / (*it);
+	//}
+	for idx, v := range list {
+		list[idx] = (p - 1) / v
+	}
+	for i = 2; i <= p-1; i++ {
+		for _, v := range list {
+			if powerMod(i, v, p) == 1 {
+				goto loopend
+			}
+		}
+		break
+	loopend:
+	}
+	return i
+}
+
+func powerMod(a, b, m int) int {
+	var i int
+	for i = 1; b != 0; b >>= 1 {
+		if b&1 != 0 {
+			i = (i * a) % m;
+		}
+		a = (a * a) % m;
+	}
+	return i;
 }
